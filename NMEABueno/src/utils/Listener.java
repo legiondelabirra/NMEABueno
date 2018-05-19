75cecbde7861f27d54f67e2328507a10637d572e
Yuki <ZeroFreedom00@gmail.com>
2018-05-19 18:24:39 +0200

    Listener completado

---
+++ NMEABueno/src/utils/Listener.java
@@ -0,0 +1,172 @@
+package utils;
+
+import javafx.beans.property.DoubleProperty;
+import javafx.beans.property.SimpleDoubleProperty;
+import net.sf.marineapi.nmea.event.AbstractSentenceListener;
+import net.sf.marineapi.nmea.io.SentenceReader;
+import net.sf.marineapi.nmea.sentence.*;
+import net.sf.marineapi.nmea.util.Measurement;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.InputStream;
+
+public class Listener {
+
+    private static Listener instance = new Listener();
+
+    private SentenceReader reader;
+
+    private Listener(){
+
+    }
+
+    public static Listener getInstance() {
+        return instance;
+    }
+
+    public void readFile(File file){
+        try {
+            InputStream stream = new FileInputStream(file);
+            reader = new SentenceReader(stream);
+            HDGSentenceListener hdg = new HDGSentenceListener();
+            reader.addSentenceListener(hdg);
+            MDASentenceListener mda = new MDASentenceListener();
+            reader.addSentenceListener(mda);
+            MWVSentenceListener mwv = new MWVSentenceListener();
+            reader.addSentenceListener(mwv);
+            XDRSentenceListener xdr = new XDRSentenceListener();
+            reader.addSentenceListener(xdr);
+            RMCSentenceListener rmc = new RMCSentenceListener();
+            reader.addSentenceListener(rmc);
+            reader.start();
+        }catch (Exception e){}
+
+    }
+
+    public void stopListening(){
+        if(reader == null)
+            return;
+        reader.stop();
+    }
+
+    private final DoubleProperty HEADING = new SimpleDoubleProperty();
+
+    public DoubleProperty HDGProperty(){
+        return HEADING;
+    }
+
+    class HDGSentenceListener extends AbstractSentenceListener<HDGSentence> {
+        @Override
+        public void sentenceRead(HDGSentence hdgSentence) {
+            HEADING.set(hdgSentence.getHeading());
+        }
+    }
+
+    private final DoubleProperty TWD = new SimpleDoubleProperty();
+
+    public DoubleProperty TWDProperty(){
+        return TWD;
+    }
+
+    private final DoubleProperty TWS = new SimpleDoubleProperty();
+
+    public DoubleProperty TWSProperty(){
+        return TWS;
+    }
+
+    private final DoubleProperty TEMP = new SimpleDoubleProperty();
+
+    public DoubleProperty TEMPProperty(){
+        return TEMP;
+    }
+
+    class MDASentenceListener extends AbstractSentenceListener<MDASentence> {
+        @Override
+        public void sentenceRead(MDASentence mdaSentence) {
+            TWD.set(mdaSentence.getTrueWindDirection());
+            TWS.set(mdaSentence.getWindSpeed());
+            TEMP.set(mdaSentence.getAirTemperature());
+        }
+    }
+
+    private final DoubleProperty AWA = new SimpleDoubleProperty();
+
+    public DoubleProperty AWAProperty(){
+        return AWA;
+    }
+
+    private final DoubleProperty AWS = new SimpleDoubleProperty();
+
+    public DoubleProperty AWSProperty(){
+        return AWS;
+    }
+
+    class MWVSentenceListener extends AbstractSentenceListener<MWVSentence> {
+        @Override
+        public void sentenceRead(MWVSentence mwvSentence) {
+            AWA.set(mwvSentence.getAngle());
+            AWS.set(mwvSentence.getSpeed());
+        }
+    }
+
+    private final DoubleProperty PITCH = new SimpleDoubleProperty();
+
+    public DoubleProperty PITCHProperty(){
+        return PITCH;
+    }
+
+    private final DoubleProperty ROLL = new SimpleDoubleProperty();
+
+    public DoubleProperty ROLLProperty(){
+        return ROLL;
+    }
+
+    class XDRSentenceListener extends AbstractSentenceListener<XDRSentence> {
+        @Override
+        public void sentenceRead(XDRSentence xdrSentence) {
+            for(Measurement m : xdrSentence.getMeasurements()){
+                if(m.getName().equals("PTCH")){  ///no se si va una I
+                    PITCH.set(m.getValue());
+                }
+                if(m.getName().equals("ROLL")){
+                    ROLL.set(m.getValue());
+                }
+            }
+        }
+    }
+
+    private final DoubleProperty LAT = new SimpleDoubleProperty();
+
+    public DoubleProperty LATProperty(){
+        return LAT;
+    }
+
+    private final DoubleProperty LON = new SimpleDoubleProperty();
+
+    public DoubleProperty LONProperty(){
+        return LON;
+    }
+
+    private final DoubleProperty COG = new SimpleDoubleProperty();
+
+    public DoubleProperty COGProperty(){
+        return COG;
+    }
+
+    private final DoubleProperty SOG = new SimpleDoubleProperty();
+
+    public DoubleProperty SOGProperty(){
+        return SOG;
+    }
+
+    class RMCSentenceListener extends AbstractSentenceListener<RMCSentence> {
+        @Override
+        public void sentenceRead(RMCSentence rmcSentence) {
+            SOG.set(rmcSentence.getSpeed());
+            COG.set(rmcSentence.getCourse());
+            LAT.set(rmcSentence.getPosition().getLatitude());
+            LON.set(rmcSentence.getPosition().getLongitude());
+        }
+    }
+}
